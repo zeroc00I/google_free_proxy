edcee14a30880eec1fd1a6990dfa1efb9fe3019b
Bruno - Zeroc00I <b.minossi@gmail.com>
2024-02-28 18:22:21 -0300

    Update google_proxy.py

--- google_proxy.py
+++ google_proxy.py
@@ -6,14 +6,16 @@ import urllib3
 import json
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 def extract_value_from_host(host):
     try:
         #print(f"Fetching {host}...")
         response = requests.get("https://docs.google.com/gview?url="+host+"&embedded=true",verify=False)
         if response.status_code == 200:
             token =  response.text.split('"')[-12].replace("meta?id\\u003d","")
-            get_content_with_token_from_google(token)
-
+            for page in range(31):  # Permuta de 0 a 30
+                if not get_content_with_token_from_google(token, page):
+                    break  # Interrompe a iteração se a solicitação falhar
         """
         else:
             print(f"[-] Failed to fetch {host}. Status code: {response.status_code}")
@@ -21,11 +23,19 @@ def extract_value_from_host(host):
     except Exception as e:
         print(f"An error occurred while processing {host}: {e}")
 
-def get_content_with_token_from_google(token):
-    response = requests.get(f"https://docs.google.com/viewerng/text?id={token}&authuser=0&page=0",verify=False)
-    final_response = json.loads(response.text.replace(")]}'",""))["data"].replace("\n","")
-    #print(f"[+] Final response {final_response}")
-    print(f"{final_response}")
+def get_content_with_token_from_google(token, page):
+    try:
+        response = requests.get(f"https://docs.google.com/viewerng/text?id={token}&authuser=0&page={page}", verify=False)
+        if response.status_code == 200:
+            final_response = json.loads(response.text.replace(")]}'", ""))["data"].replace("\n", "")
+            print(f"{final_response}")
+            return True
+        else:
+            print(f"[-] Failed to fetch page {page} for token {token}. Status code: {response.status_code}")
+            return False
+    except Exception as e:
+        print(f"An error occurred while fetching page {page} for token {token}: {e}")
+        return False
 
 def main():
     parser = argparse.ArgumentParser(description='Extract values from HTML tags in a list of hosts.')
